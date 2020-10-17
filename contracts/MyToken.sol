3c570029ac7c9d621882273601623ef15f77dbb1
Guy Vider <guy@guyvider.com>
2020-10-16 18:27:03 -0700

    set totalSupply

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.5.0 <0.8.0;
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
-  constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) public {
-    
+  constructor() ERC20("MyToken", "MTK") public {
+    _mint(msg.sender, 10000);
   }
 }
